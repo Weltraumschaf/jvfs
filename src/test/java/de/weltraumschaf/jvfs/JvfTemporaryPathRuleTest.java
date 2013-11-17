b3131c9fb43168129b37d366e9c9848a0ad3e81b
Weltraumschaf <ich@weltraumschaf.de>
2013-11-17 02:08:15 +0100

    Add tests for temp path rule.

--- src/test/java/de/weltraumschaf/jvfs/JvfTemporaryPathRuleTest.java
+++ src/test/java/de/weltraumschaf/jvfs/JvfTemporaryPathRuleTest.java
@@ -12,9 +12,15 @@
 
 package de.weltraumschaf.jvfs;
 
-import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 /**
  * Tests for {@link JvfTemporaryPathRule}.
@@ -23,8 +29,46 @@ import static org.hamcrest.Matchers.*;
  */
 public class JvfTemporaryPathRuleTest {
 
+    private final JvfTemporaryPathRule sut = new JvfTemporaryPathRule();
+
     @Test
-    public void testSomeMethod() {
+    public void beforeAndAfter() throws Throwable {
+        final JvfTemporaryPathRule spy = spy(sut);
+        spy.before();
+        verify(spy, times(1)).create();
+        spy.after();
+        verify(spy, times(1)).delete();
+    }
+
+    @Test
+    public void getRoot() throws IOException {
+        assertThat(sut.getRoot(), is(nullValue()));
+        sut.create();
+        final Path tmp = sut.getRoot();
+        assertThat(sut.getRoot(), is(not(nullValue())));
+        assertThat(Files.exists(tmp), is(true));
+        sut.delete();
+        assertThat(Files.exists(tmp), is(false));
+    }
+
+    @Test
+    public void createTemporaryFile() throws IOException {
+        sut.create();
+        final Path file = sut.newFile("foobar");
+        assertThat(Files.exists(file), is(true));
+        assertThat(Files.isDirectory(file), is(false));
+        sut.delete();
+        assertThat(Files.exists(file), is(false));
+    }
+
+    @Test
+    public void createTemporaryFolder() throws IOException {
+        sut.create();
+        final Path folder = sut.newFolder("foobar");
+        assertThat(Files.exists(folder), is(true));
+        assertThat(Files.isDirectory(folder), is(true));
+        sut.delete();
+        assertThat(Files.exists(folder), is(false));
     }
 
 }
