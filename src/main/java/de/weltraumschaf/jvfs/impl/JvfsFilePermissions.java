a71d550b5996433a5b2b28e7ac26bdab7a9f8cc0
Weltraumschaf <ich@weltraumschaf.de>
2013-11-30 01:28:29 +0100

    Fix the tests for file permissions.

--- src/main/java/de/weltraumschaf/jvfs/impl/JvfsFilePermissions.java
+++ src/main/java/de/weltraumschaf/jvfs/impl/JvfsFilePermissions.java
@@ -11,55 +11,119 @@
  */
 package de.weltraumschaf.jvfs.impl;
 
+import de.weltraumschaf.jvfs.JvfsAssertions;
 import java.nio.file.attribute.FileAttribute;
 import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Set;
 
 /**
+ * Holds the file permissions.
+ *
+ * Based on a set of {@link PosixFilePermission}.
  *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 class JvfsFilePermissions {
 
+    /**
+     * Name to identify Posix file permission attribute.
+     */
+    private static final String NAME = "posix:permissions";
+
+    /**
+     * Holds the permissions.
+     */
     private final Set<PosixFilePermission> permissions;
 
+    /**
+     * Dedicated constructor.
+     *
+     * @param permissions must not be {@code null}
+     */
     public JvfsFilePermissions(final Set<PosixFilePermission> permissions) {
         super();
+        JvfsAssertions.notNull(permissions, "permissions");
         this.permissions = permissions;
     }
 
+    /**
+     * Has owner read permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean ownerRead() {
         return permissions.contains(PosixFilePermission.OWNER_READ);
     }
 
+    /**
+     * Has owner write permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean ownerWrite() {
         return permissions.contains(PosixFilePermission.OWNER_WRITE);
     }
 
+    /**
+     * Has owner execute permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean ownerExecute() {
         return permissions.contains(PosixFilePermission.OWNER_EXECUTE);
     }
 
+    /**
+     * Has group read permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean groupRead() {
         return permissions.contains(PosixFilePermission.GROUP_READ);
     }
 
+    /**
+     * Has group write permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean groupWrite() {
         return permissions.contains(PosixFilePermission.GROUP_WRITE);
     }
 
+    /**
+     * Has group execute permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean groupExecute() {
         return permissions.contains(PosixFilePermission.GROUP_EXECUTE);
     }
 
+    /**
+     * Has others read permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean othersRead() {
         return permissions.contains(PosixFilePermission.OTHERS_READ);
     }
 
+    /**
+     * Has others write permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean othersWrite() {
         return permissions.contains(PosixFilePermission.OTHERS_WRITE);
     }
 
+    /**
+     * Has others execute permission.
+     *
+     * @return {@code true} if permitted, else {@code false}
+     */
     boolean othersExecute() {
         return permissions.contains(PosixFilePermission.OTHERS_EXECUTE);
     }
@@ -76,24 +140,25 @@ class JvfsFilePermissions {
         }
 
         final JvfsFilePermissions other = (JvfsFilePermissions) obj;
-        return permissions.equals(other);
+        return permissions.equals(other.permissions);
     }
 
     @Override
     public String toString() {
-        return permissions.toString();
+        return PosixFilePermissions.toString(permissions);
     }
 
     static JvfsFilePermissions forValue(final FileAttribute<?>... attributes) {
         for (final FileAttribute<?> attribute : attributes) {
-            if (attribute instanceof Set) {
-                final Set permissions = (Set) attribute;
+            final String name = attribute.name();
+            final Object value = attribute.value();
 
-                if (!permissions.isEmpty() && permissions.toArray()[0] instanceof PosixFilePermission) {
-                    return new JvfsFilePermissions(permissions);
-                }
+            if (NAME.equals(name) && value instanceof Set) {
+                return new JvfsFilePermissions((Set) value);
             }
         }
+
         return null;
     }
+
 }
--- src/test/java/de/weltraumschaf/jvfs/impl/JvfsFilePermissionsTest.java
+++ src/test/java/de/weltraumschaf/jvfs/impl/JvfsFilePermissionsTest.java
@@ -1,32 +1,261 @@
 /*
  *  LICENSE
- * 
+ *
  * "THE BEER-WARE LICENSE" (Revision 43):
  * "Sven Strittmatter" <weltraumschaf@googlemail.com> wrote this file.
  * As long as you retain this notice you can do whatever you want with
  * this stuff. If we meet some day, and you think this stuff is worth it,
  * you can buy me a non alcohol-free beer in return.
- * 
+ *
  * Copyright (C) 2012 "Sven Strittmatter" <weltraumschaf@googlemail.com>
  */
-
 package de.weltraumschaf.jvfs.impl;
 
-import org.junit.Test;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
+import java.util.Set;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.*;
+import org.junit.Test;
 
 /**
+ * Tests for {@link JvfsFilePermissions}.
  *
  * @author Sven Strittmatter <weltraumschaf@googlemail.com>
  */
 public class JvfsFilePermissionsTest {
 
-    public JvfsFilePermissionsTest() {
+    @Test
+    public void defaults() {
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(new HashSet<PosixFilePermission>()));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
     }
 
     @Test
-    public void testSomeMethod() {
+    public void ownerRead() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OWNER_READ);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(true));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void ownerWrite() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OWNER_WRITE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(true));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void ownerExecute() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OWNER_EXECUTE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(true));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void groupRead() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.GROUP_READ);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(true));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void groupWrite() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.GROUP_WRITE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(true));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void groupExecute() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.GROUP_EXECUTE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(true));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void othersRead() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OTHERS_READ);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(true));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void othersWrite() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OTHERS_WRITE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(true));
+        assertThat(sut.othersExecute(), is(false));
+    }
+
+    @Test
+    public void othersExecute() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+        final JvfsFilePermissions sut = JvfsFilePermissions.forValue(
+                PosixFilePermissions.asFileAttribute(permissions));
+        assertThat(sut.ownerRead(), is(false));
+        assertThat(sut.ownerWrite(), is(false));
+        assertThat(sut.ownerExecute(), is(false));
+        assertThat(sut.groupRead(), is(false));
+        assertThat(sut.groupWrite(), is(false));
+        assertThat(sut.groupExecute(), is(false));
+        assertThat(sut.othersRead(), is(false));
+        assertThat(sut.othersWrite(), is(false));
+        assertThat(sut.othersExecute(), is(true));
+    }
+
+    @Test
+    public void testHashCode() {
+        final Set<PosixFilePermission> permissions1 = new HashSet<PosixFilePermission>();
+        permissions1.add(PosixFilePermission.OWNER_READ);
+        permissions1.add(PosixFilePermission.OWNER_WRITE);
+        final Set<PosixFilePermission> permissions2 = new HashSet<PosixFilePermission>();
+        permissions2.add(PosixFilePermission.OWNER_WRITE);
+        final JvfsFilePermissions sut1 = new JvfsFilePermissions(permissions1);
+        final JvfsFilePermissions sut2 = new JvfsFilePermissions(permissions1);
+        final JvfsFilePermissions sut3 = new JvfsFilePermissions(permissions2);
+
+        assertThat(sut1.hashCode(), is(sut1.hashCode()));
+        assertThat(sut1.hashCode(), is(sut2.hashCode()));
+        assertThat(sut2.hashCode(), is(sut1.hashCode()));
+        assertThat(sut2.hashCode(), is(sut2.hashCode()));
+
+        assertThat(sut3.hashCode(), is(sut3.hashCode()));
+        assertThat(sut3.hashCode(), is(not(sut1.hashCode())));
+        assertThat(sut3.hashCode(), is(not(sut2.hashCode())));
+    }
+
+    @Test
+    public void equals() {
+        final Set<PosixFilePermission> permissions1 = new HashSet<PosixFilePermission>();
+        permissions1.add(PosixFilePermission.OWNER_READ);
+        permissions1.add(PosixFilePermission.OWNER_WRITE);
+        final Set<PosixFilePermission> permissions2 = new HashSet<PosixFilePermission>();
+        permissions2.add(PosixFilePermission.OWNER_WRITE);
+        final JvfsFilePermissions sut1 = new JvfsFilePermissions(permissions1);
+        final JvfsFilePermissions sut2 = new JvfsFilePermissions(permissions1);
+        final JvfsFilePermissions sut3 = new JvfsFilePermissions(permissions2);
+
+        //CHECKSTYLE:OFF
+        assertThat(sut1.equals(null), is(false));
+        assertThat(sut1.equals(""), is(false));
+        //CHECKSTYLE:ON
+
+        assertThat(sut1.equals(sut1), is(true));
+        assertThat(sut1.equals(sut2), is(true));
+        assertThat(sut2.equals(sut1), is(true));
+        assertThat(sut2.equals(sut2), is(true));
+
+        assertThat(sut3.equals(sut3), is(true));
+        assertThat(sut3.equals(sut1), is(false));
+        assertThat(sut3.equals(sut2), is(false));
+    }
+
+    @Test
+    public void testToString() {
+        final Set<PosixFilePermission> permissions = new HashSet<PosixFilePermission>();
+        assertThat(new JvfsFilePermissions(permissions).toString(), is(equalTo("---------")));
+        permissions.add(PosixFilePermission.OWNER_READ);
+        permissions.add(PosixFilePermission.OWNER_WRITE);
+        assertThat(new JvfsFilePermissions(permissions).toString(), is(equalTo("rw-------")));
+        permissions.add(PosixFilePermission.GROUP_EXECUTE);
+        assertThat(new JvfsFilePermissions(permissions).toString(), is(equalTo("rw---x---")));
+        permissions.add(PosixFilePermission.OTHERS_READ);
+        permissions.add(PosixFilePermission.OTHERS_WRITE);
+        permissions.add(PosixFilePermission.OTHERS_EXECUTE);
+        assertThat(new JvfsFilePermissions(permissions).toString(), is(equalTo("rw---xrwx")));
     }
 
 }
